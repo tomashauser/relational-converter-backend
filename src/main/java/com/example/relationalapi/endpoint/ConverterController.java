dfe8fb8c36d280db818c59ed227d323e062ea6a7
hauseto2 <hauseto2@fel.cvut.cz>
2022-03-20 12:20:48 +0100

    added origins parameter to cross origin annotation

--- src/main/java/com/example/relationalapi/endpoint/ConverterController.java
+++ src/main/java/com/example/relationalapi/endpoint/ConverterController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/convert")
 public class ConverterController {
     @Autowired
