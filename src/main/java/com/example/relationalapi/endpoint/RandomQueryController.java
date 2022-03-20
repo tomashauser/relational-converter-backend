c77fdb4b1704c33264e15e6b79d0253262b3789a
hauseto2 <hauseto2@fel.cvut.cz>
2022-03-20 12:10:13 +0100

    changed to cross origin

--- src/main/java/com/example/relationalapi/endpoint/RandomQueryController.java
+++ src/main/java/com/example/relationalapi/endpoint/RandomQueryController.java
@@ -3,10 +3,12 @@ package com.example.relationalapi.endpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class RandomQueryController {
     @Autowired
